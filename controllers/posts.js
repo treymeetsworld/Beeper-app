66a6a87f2422b37c210f8ab54c651ac0afb19afd
Trevor Mwangi <trevorzac16@yahoo.com>
2021-11-09 06:37:56 -0500

    Bug fix

--- controllers/posts.js
+++ controllers/posts.js
@@ -19,9 +19,7 @@ function create(req,res){
   const post = new Post(req.body)
   post.save(function(err){
     if (err) return res.redirect('/posts')
-    res.render('posts/index',{
-      post: post,
-    })
+    res.redirect('/posts')
   })
 }
 
