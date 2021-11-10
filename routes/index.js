f45867c0baf1d748e812736bbf3fc7a0e488f9ad
Trevor Mwangi <trevorzac16@yahoo.com>
2021-11-10 14:37:05 -0500

    Tweak css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'BEEPER', user: req.user ? req.user : null})
+  res.redirect('posts/timeline' )
 })
 
 export {
