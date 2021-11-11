1e239f4e7d626aaf7410269fd17ef9641a512ace
Trevor Mwangi <trevorzac16@yahoo.com>
2021-11-11 18:21:27 -0500

    Add authorization to views

--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,24 @@
 import { Router } from 'express'
 import * as postsCtrl from '../controllers/posts.js'
+import { passUserToView } from '../middleware/middleware.js'
 const router = Router()
 
-router.get('/', postsCtrl.index)
-router.get('/timeline', postsCtrl.timeline)
-router.get('/new', postsCtrl.new)
-router.post('/', postsCtrl.create)
-router.get('/:id', postsCtrl.show)
-router.put('/:id', postsCtrl.update)
-router.delete('/:id', postsCtrl.delete)
-router.post('/:id/replies', postsCtrl.createReply)
+router.get('/',passUserToView, postsCtrl.index)
+router.get('/timeline',isLoggedIn,passUserToView, postsCtrl.timeline)
+router.get('/new',isLoggedIn, postsCtrl.new)
+router.post('/',isLoggedIn, postsCtrl.create)
+router.get('/:id',isLoggedIn,passUserToView, postsCtrl.show)
+router.put('/:id',isLoggedIn, postsCtrl.update)
+router.delete('/:id',isLoggedIn, postsCtrl.delete)
+router.post('/:id/replies',isLoggedIn, postsCtrl.createReply)
 
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next()
+  res.redirect('/auth/google')
+}
 
 export {
   router
 }
+
