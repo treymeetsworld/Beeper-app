981b45e52fcd23734d02ef4d272c01adbd537ead
Trevor Mwangi <trevorzac16@yahoo.com>
2022-01-26 06:31:42 -0500

    add sass

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,7 +29,7 @@ function create(req,res){
   const post = new Post(req.body)
   post.save(function(err){
     if (err) return res.redirect('/posts')
-    res.redirect('/posts')
+    res.redirect('posts/timeline')
   })
 }
 
